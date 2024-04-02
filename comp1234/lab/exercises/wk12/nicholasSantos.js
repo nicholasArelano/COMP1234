1445f10a00e8006e5b4580ff04724fb7320674c6
Nicholas <nicholas@Nicholass-MacBook-Pro.local>
2024-04-02 16:43:24 -0400

    message

---
+++ comp1234/lab/exercises/wk12/nicholasSantos.js
@@ -0,0 +1,42 @@
+//variables
+let total =0;
+let operation = '';
+
+
+//input from user
+let num1 = parseFloat(prompt("Choose one number:"));
+let num2= parseFloat(prompt("Choose another number:"));
+
+//Solving
+operation = prompt("Choose an arithmetic operation ( +. -, *, / )");
+    if (operation !=='+' && operation!=='-' && operation!== '*' && operation!== '/') {
+       console.log("Invalid Arithmetic Operation!");
+    } else {
+        function calculate(num1, num2, operation) {
+            switch (operation) {
+                case '+':
+                    return num1 + num2;
+
+                case '-':
+                    return num1 - num2;
+
+                case '*':
+                    return num1 * num2;
+
+                case '/':
+                    if (num2 !== 0) {
+                        return num1 / num2;
+                    } else {
+                        return 'Error: Division by zero';
+                    }
+                default:
+                    return 'Error: Invalid operation';
+            }
+
+
+        }
+
+        total = calculate(num1, num2, operation);
+//Presenting
+        document.write(num1 + " " + operation + " " + num2 + " = " + total);
+    }
